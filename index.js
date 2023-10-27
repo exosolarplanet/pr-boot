23c9f8c825069e4460869d87a8c2c128b3d24c30
exosolarplanet <ecedenniz@gmail.com>
2023-10-27 21:27:37 +0100

    update inputs/outputs

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const imageName = core.getInput('image-name');
+  console.log(`Image name is: ${imageName}!`);
+
+  const imageVersion = core.getInput('image-version');
+  console.log(`Image version is: ${imageVersion}!`);
+
+  const artifactoryPath = core.getInput('artifactory-path');
+  console.log(`Artifactory path is: ${artifactoryPath}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
