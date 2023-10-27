90c85d0bc9cba3259a362b4609b506ee5259e7a8
exosolarplanet <ecedenniz@gmail.com>
2023-10-27 21:32:56 +0100

    add octokit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require('octokit');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const imageName = core.getInput('image-name');
-  console.log(`Image name is: ${imageName}!`);
+  console.log(`Image name is: ${imageName}`);
 
   const imageVersion = core.getInput('image-version');
-  console.log(`Image version is: ${imageVersion}!`);
+  console.log(`Image version is: ${imageVersion}`);
 
   const artifactoryPath = core.getInput('artifactory-path');
   console.log(`Artifactory path is: ${artifactoryPath}`);
